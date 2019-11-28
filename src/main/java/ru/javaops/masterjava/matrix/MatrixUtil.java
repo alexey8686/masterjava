f2f769a12321eb6cf193446bdd984a066e07e293
Alexey <alexey8686@gmail.com>
2019-11-28 23:03:27 +0300

    hw01 cuncurrent multiply

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -3,6 +3,8 @@ package ru.javaops.masterjava.matrix;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * gkislin
@@ -10,10 +12,44 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
+
     // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
-        final int matrixSize = matrixA.length;
-        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+         class Multi implements Runnable {
+            final int N;
+            final int [][] a;
+            final int [][] b;
+            final int [][] c;
+            final int i;
+            final int j;
+
+            public Multi(int N, int i, int j, int[][] a, int[][] b, int[][] c){
+                this.N=N;
+                this.i=i;
+                this.j=j;
+                this.a=a;
+                this.b=b;
+                this.c=c;
+            }
+
+            @Override
+            public void run() {
+                for(int k = 0; k < N; k++)
+                    c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+        executor = Executors.newFixedThreadPool(10);
+        int N = matrixA.length;
+        final  int[][] matrixC = new int[1000][1000];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                executor.submit(new Multi(N,i,j,matrixA,matrixB,matrixC));
+            }
+        }
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+
 
         return matrixC;
     }
