5f04dfc8de3fb19ad03bf983bc2ebe9276670f68
alexey <alexey8686@gmail.com>
2020-01-16 13:45:57 +0300

    with changed maven structure v1

---
+++ test/src/main/java/ru/javaops/masterjava/matrix/MatrixBenchmark.java
@@ -0,0 +1,97 @@
+package ru.javaops.masterjava.matrix;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Timeout;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@BenchmarkMode({Mode.SingleShotTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Threads(1)
+@Fork(10)
+@Timeout(time = 5, timeUnit = TimeUnit.MINUTES)
+public class MatrixBenchmark {
+
+    // Matrix size
+    private static final int MATRIX_SIZE = 1000;
+
+    @Param({"3", "4", "10"})
+    private int threadNumber;
+
+    private static int[][] matrixA;
+    private static int[][] matrixB;
+
+    @Setup
+    public void setUp() {
+        matrixA = MatrixUtil.create(MATRIX_SIZE);
+        matrixB = MatrixUtil.create(MATRIX_SIZE);
+    }
+
+    private ExecutorService executor;
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(MatrixBenchmark.class.getSimpleName())
+                .threads(1)
+                .forks(10)
+                .timeout(TimeValue.minutes(5))
+                .build();
+        new Runner(options).run();
+    }
+
+    //    @Benchmark
+    public int[][] singleThreadMultiplyOpt() throws Exception {
+        return MatrixUtil.singleThreadMultiplyOpt(matrixA, matrixB);
+    }
+
+    //    @Benchmark
+    public int[][] singleThreadMultiplyOpt2() throws Exception {
+        return MatrixUtil.singleThreadMultiplyOpt(matrixA, matrixB);
+    }
+
+    @Benchmark
+    public int[][] concurrentMultiplyStreams() throws Exception {
+        return MatrixUtil.concurrentMultiplyStreams(matrixA, matrixB, threadNumber);
+    }
+
+    //    @Benchmark
+    public int[][] concurrentMultiply() throws Exception {
+        return MatrixUtil.concurrentMultiply(matrixA, matrixB, executor);
+    }
+
+    @Benchmark
+    public int[][] concurrentMultiply2() throws Exception {
+        return MatrixUtil.concurrentMultiply2(matrixA, matrixB, executor);
+    }
+
+    @Setup
+    public void setup() {
+        executor = Executors.newFixedThreadPool(threadNumber);
+    }
+
+    @TearDown
+    public void tearDown() {
+        executor.shutdown();
+    }
+}
---
+++ upload/src/main/java/ru/javaops/masterjava/xml/util/XsltProcessor.java
@@ -0,0 +1,56 @@
+package ru.javaops.masterjava.xml.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class XsltProcessor {
+    private static TransformerFactory FACTORY = TransformerFactory.newInstance();
+    private final Transformer xformer;
+
+    public XsltProcessor(InputStream xslInputStream) {
+        this(new BufferedReader(new InputStreamReader(xslInputStream, StandardCharsets.UTF_8)));
+    }
+
+    public XsltProcessor(Reader xslReader) {
+        try {
+            Templates template = FACTORY.newTemplates(new StreamSource(xslReader));
+            xformer = template.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            throw new IllegalStateException("XSLT transformer creation failed: " + e.toString(), e);
+        }
+    }
+
+    public String transform(InputStream xmlInputStream) throws TransformerException {
+        StringWriter out = new StringWriter();
+        transform(xmlInputStream, out);
+        return out.getBuffer().toString();
+    }
+
+    public void transform(InputStream xmlInputStream, Writer result) throws TransformerException {
+        transform(new BufferedReader(new InputStreamReader(xmlInputStream, StandardCharsets.UTF_8)), result);
+    }
+
+    public void transform(Reader sourceReader, Writer result) throws TransformerException {
+        xformer.transform(new StreamSource(sourceReader), new StreamResult(result));
+    }
+
+    public static String getXsltHeader(String xslt) {
+        return "<?xml-stylesheet type=\"text/xsl\" href=\"" + xslt + "\"?>\n";
+    }
+
+    public void setParameter(String name, String value) {
+        xformer.setParameter(name, value);
+    }
+}
